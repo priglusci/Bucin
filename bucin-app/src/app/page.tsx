1e67b501f2bd18e777b8ce9468d3b12afaa52d4e
Rezky Mandar Suaib <rezkymndr@gmail.com>
2025-03-10 02:59:51 +0800

    setup

--- bucin-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="w-full min-h-screen bg-white">
-      <div className="text-black">asdasd</div>
-    </div>
-  );
-}
